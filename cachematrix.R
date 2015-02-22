3a37592fd599e004ea27093ee65602da37f33784
dddbbb <kn.efremov@gmail.com>
2015-02-22 16:58:51 +0300

    1234

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Function return martrix. It has methods for getting and setting value for cache for environment outside the function
 
 makeCacheMatrix <- function(x = matrix()) {
         m_cache <- NULL
@@ -19,17 +19,17 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## Function returns cached value if it has cache at environment and solved value if it has not cache (and set cache at environment)
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         m <- x$getcache()
         if(!is.null(m)) {
                 message("getting cached data")
-                return(m)
+                return(m) ## Return cache
         }
         data <- x$get()
-        m <- solve(data)
-        x$setmean(m)
+        m <- solve(data) ## Solve invserse
+        x$setcache(m)  ## set value to cache
         m
 }
